6cf939bd625524767b29556eeeef427e5781dac6
Nicolas McCurdy <thenickperson@gmail.com>
2012-01-20 15:25:18 -0500

    inject.js updated, now displays an awesome face to the right of all password fields

--- inject.js
+++ inject.js
@@ -1,7 +1,6 @@
-$("input[type=password]").parent().append("<div id='derp'>herp</div>")
-//$("#derp").css("position","absolute")
-//$("#derp").css("right","0px")
-//$("#derp").css("top","0px")
-//$("#derp").css("z-index","9001")
-$("#derp").show()
-console.log("stuff is done")
+var test_element = '<img src="awesome.png" class="test" height="18px">'
+$("input[type=password]").parent().append(test_element)
+$(".test").css("position","relative")
+$(".test").css("top","3px")
+//Note that 5px is the border + padding of the input that the test element was binded to (assuming that our test element has the same margin size). We must create a for loop in JavaScript in the future to programmatically grab the margin, border, and padding of each password input field as the test element is binded to it.
+console.log("test element added to page")
