a07df27c0206cf6111a764957d8e8990e92efa33
Daniel Wallach <dywallach@dywallach.net>
2012-01-20 19:21:56 -0500

    should be working, but is not setting at correct location

--- inject.js
+++ inject.js
@@ -11,15 +11,23 @@ var intHeight = parseInt(height);
 var intWidth = parseInt(width);
 
 var newleft = position.left + intWidth - intHeight;
+console.log(position.left);
+console.log(intWidth);
+console.log(intHeight);
 console.log(newleft);
 //make test elements
-var test_element = '<img src="chrome-extension://mbomcppphdldjpnoekfomlhcdiegncjc/awesome.png" class="test" height="'+ height + '">';
+var test_element = '<img src="chrome-extension://mbomcppphdldjpnoekfomlhcdiegncjc/awesome.png" class="test" height="'+ height + '" left=' + newleft + '">';
 
 //set position
-$(".test").css("position","absolute");
+$(".test").css("position","relative");
 $("body").append(test_element);
 $(".test").css("left",newleft + "px");
 $(".test").css("top",position.top + "px");
+
+var pos = $(".test").offset();
+console.log("-----------------")
+console.log(pos.left);
+console.log(pos.top);
 
 //Note that 5px is the border + padding of the input that the test element was binded to (assuming that our test element has the same margin size). We must create a for loop in JavaScript in the future to programmatically grab the margin, border, and padding of each password input field as the test element is binded to it.
 console.log("test element added to page");
