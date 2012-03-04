3008b758ff7f322ec5305885a9c21e23dabe021e
Brian Wyant <wyantb@gmail.com>
2012-03-04 14:14:07 -0500

    Hashmask should work for password fields added after document.ready.

--- inject.js
+++ inject.js
@@ -1,3 +1,10 @@
-$(document).ready(function(){
-  var pars = $("input[type=password]").hashmask();
+
+// Alternative method to inject hashmask: use jcade, do it for current and future password fields
+$(document).create("input[type=password]", function( ev ) {
+  $(ev.target).hashmask();
 });
+
+// Basic way to inject hashmask: do it for password fields on document.ready
+//$(document).ready(function(){
+//  var pars = $("input[type=password]").hashmask();
+//});
