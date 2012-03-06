dd5a3b3e4be31578fc34fb2d7c6ebdcb9923d08d
Brian Wyant <wyantb@gmail.com>
2012-03-06 18:38:42 -0500

    Now included: choose one of three hash functions!

--- bg.js
+++ bg.js
@@ -1,11 +1,18 @@
-function getText(){
-  return localStorage.getItem("text");
-}
+
+// Enter the defaults for salt and hash, if none exist
+if (localStorage.salt == undefined) localStorage.salt = "#e" + Math.random();
+if (localStorage.hash == undefined) localStorage.hash = "sha256";
 
 chrome.extension.onRequest.addListener(onReceiveEvent);
 
 function onReceiveEvent(data, sender, callback){
-  if (data.eventName == "getText"){
-    callback({value: getText()});
+  if (data.eventName == "settings") {
+
+    callback({
+      salt: localStorage.salt,
+      hash: localStorage.hash
+    });
+
   }
 }
+
--- inject.js
+++ inject.js
@@ -1,7 +1,16 @@
 
-// Alternative method to inject hashmask: use jcade, do it for current and future password fields
-$(document).create("input[type=password]", function( ev ) {
-  $(ev.target).hashmask();
+// Send a request to our BG page for all settings before injecting hashmask
+chrome.extension.sendRequest({eventName: "settings"}, function (result) {
+
+  // Update hash algorithm about to be used by hashmask
+  $.hashmask.settings.hashUsed = result.hash;
+  $.hashmask.settings.hashFunction = $.hashmask.hashAlgorithms[result.hash];
+
+  // Alternative method to inject hashmask: use jcade, do it for current and future password fields
+  $(document).create("input[type=password]", function( ev ) {
+    $(ev.target).hashmask();
+  });
+
 });
 
 // Basic way to inject hashmask: do it for password fields on document.ready
