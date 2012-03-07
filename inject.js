028f4e8651d0c45c1efff2cbcf503af5c727fada
Brian Wyant <wyantb@gmail.com>
2012-03-06 20:11:12 -0500

    If the password field gets destroyed, remove hashmask.

--- inject.js
+++ inject.js
@@ -7,8 +7,14 @@ chrome.extension.sendRequest({eventName: "settings"}, function (result) {
   $.hashmask.settings.hashFunction = $.hashmask.hashAlgorithms[result.hash];
 
   // Alternative method to inject hashmask: use jcade, do it for current and future password fields
-  $(document).create("input[type=password]", function( ev ) {
-    $(ev.target).hashmask();
+  $(document).create("input[type=password]", function (ev) {
+    var hashmask = $(ev.target).hashmask();
+
+    // Get rid of the hashmask if the target gets destroyed - though, when does this happen?
+    $(ev.target).destroy( function (ev) {
+      console.log("Password field getting destroyed - removing hashmask.");
+      $(hashmask).remove();
+    });
   });
 
 });
