26dbb23f9e0949222e4eb0bb484b1c5b878b8294
Daniel Wallach <dywallach@dywallach.net>
2012-03-19 17:29:07 -0400

    some salt stuff, and switching branch is good

--- inject.js
+++ inject.js
@@ -4,6 +4,7 @@ chrome.extension.sendRequest({eventName: "settings"}, function (result) {
   // Update hash algorithm about to be used by hashmask
   $.hashmask.settings.hashUsed = result.hash;
   $.hashmask.settings.hashFunction = $.hashmask.hashAlgorithms[result.hash];
+	$.hashmask.settings.salt = result.salt;
 
   // Alternative method to inject hashmask: use jcade, do it for current and future password fields
   $(document).create("input[type=password]", function (ev) {
