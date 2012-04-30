c5d63f8af93c327caf357d1d54b6abc537012180
Brian Wyant <wyantb@gmail.com>
2012-04-30 19:11:55 -0400

    And compute width a bit better, including minimum.

--- src/hashmask.js
+++ src/hashmask.js
@@ -107,13 +107,21 @@
     };
 
     updateDivPos = function ($this, $sparkline) {
+      // Compute the height
       var height = $this.outerHeight() - 5 - 
         parseInt($this.css('borderBottomWidth'), 10) - 
         parseInt($this.css('borderTopWidth'), 10);
+
+      // Compute the width
       var width = $this.outerWidth();
         $this.css('borderLeftWidth'), 10;
         $this.css('borderRightWidth'), 10;
 
+      // But keep it in the range of 50-100
+      width = Math.min(100, width / 2);
+      width = Math.max(50, width);
+
+      // And configure the sparkline location accordingly
       $sparkline.css({
         position:    'absolute',
         top:         $this.offset().top + 2.5 + 
@@ -122,14 +130,14 @@
         left:        $this.offset().left + 
                         $this.outerWidth() - 
                         parseInt($this.css('borderRightWidth'), 10) - 
-                        parseInt(settings.sparklineOptions.width, 10),
+                        parseInt(width, 10),
 
-        width:       Math.min(100, width/2),
+        width:       width,
         height:      height,
         'z-index':   9001
       });
 
-      return [height, Math.min(100, width/2)];
+      return [height, width];
     };
 
     /**
