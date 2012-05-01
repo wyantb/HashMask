97645f21adddbebe79b7b08d7a2167150356e431
Brian Wyant <wyantb@gmail.com>
2012-05-01 14:43:09 -0400

    Prepare for a popup browser action.

---
+++ html/popup.js
@@ -0,0 +1,21 @@
+/**
+ * HashMask - an old approach to password masking, in the browser.
+ *
+ * REQUIRES:
+ * jquery-1.7.2.js
+ * bootstrap.js
+ * src/inject.js (and its requirements)
+ *
+ * @author    Society of Software Engineers (http://sse.se.rit.edu)
+ * @author    Brian Wyant <wyantb@gmail.com>
+ * @license   http://www.opensource.org/licenses/bsd-license.php
+ *
+**/
+
+$("#enable").click(function (ev) {
+  console.log("Enable!");
+});
+
+$("#disable").click(function (ev) {
+  console.log("Disable!");
+});
