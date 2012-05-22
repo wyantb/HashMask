15063bb8d6c829ee3b041e5f5d3f1a3986edeb33
Brian Wyant <wyantb@gmail.com>
2012-05-22 11:50:58 -0400

    In theory, should work now.

--- src/inject.js
+++ src/inject.js
@@ -13,37 +13,17 @@
  *
 **/
 
-if (typeof chrome !== "undefined" && 
-    typeof chrome.extension !== "undefined" && 
-    typeof chrome.extension.sendRequest !== "undefined") {
+self.port.on("getSettings", function (settings) {
+  // Update hash algorithm about to be used by hashmask
+  $.hashmask.settings.hashUsed = settings.hash;
+  $.hashmask.settings.hashFunction = $.hashmask.hashAlgorithms[ss.storage.hash];
+  $.hashmask.settings.salt = settings.salt;
+  $.hashmask.settings.sparkInterval = settings.delay;
 
-    // Update hash algorithm about to be used by hashmask
-    $.hashmask.settings.hashUsed = ss.storage.hash;
-    $.hashmask.settings.hashFunction = $.hashmask.hashAlgorithms[ss.storage.hash];
-    $.hashmask.settings.salt = ss.storage.salt;
-    $.hashmask.settings.sparkInterval = ss.storage.delay;
-  
-    // Alternative method to inject hashmask: use jcade, do it for current and future password fields
-    $(document).create("input[type=password]", function (ev) {
-      $(ev.target).hashmask();
-    });
-  
-} 
+  // Alternative method to inject hashmask: use jcade, do it for current and future password fields
+  $(document).create("input[type=password]", function (ev) {
+    $(ev.target).hashmask();
+  });
+});
 
-// Should only occur when inject isn't in Chrome extension
-else {
-  if (ss.storage.salt == undefined) {
-    var randomWords = sjcl.random.randomWords(6, 0);
-    var salt = sjcl.codec.base64.fromBits(randomWords);
-    ss.storage.salt = salt;
-  }
-  if (ss.storage.hash == undefined) ss.storage.hash = "sha256";
-  if (ss.storage.delay == undefined) ss.storage.delay = 200;
-  $.hashmask.settings.salt = ss.storage.salt;
-  $.hashmask.settings.hashUsed = ss.storage.hash;
-  $.hashmask.settings.hashFunction = $.hashmask.hashAlgorithms["sha256"];
-  $.hashmask.settings.sparkInterval = ss.storage.delay;
-  
-  $("input[type=password]").hashmask();
-}
-
+self.port.emit("getSettings", {});
